facfe769a5ac6c7e10df809d2eaf6e9575f9f82f
David <dnash@cern.ch>
2015-09-27 22:50:38 +0200

    UpgradeSim Config

--- SLHCUpgradeSimulations/Configuration/python/me0Customs.py
+++ SLHCUpgradeSimulations/Configuration/python/me0Customs.py
@@ -81,6 +81,18 @@ def customise_RecoFull(process):
     process=outputCustoms(process)
     return process
 
+
+def customise_Reco(process):
+    #process.load('RecoLocalMuon.GEMRecHit.gemRecHits_cfi')
+    process.load('RecoLocalMuon.GEMRecHit.me0RecHits_cfi')
+    process.muonlocalreco += process.me0RecHits
+    process.standAloneMuons.STATrajBuilderParameters.FilterParameters.EnableME0Measurement = cms.bool(True)
+    process.standAloneMuons.STATrajBuilderParameters.BWFilterParameters.EnableME0Measurement = cms.bool(True)
+    process.refittedStandAloneMuons.STATrajBuilderParameters.FilterParameters.EnableME0Measurement = cms.bool(True)
+    process.refittedStandAloneMuons.STATrajBuilderParameters.BWFilterParameters.EnableME0Measurement = cms.bool(True)
+    process=outputCustoms(process)
+    return process
+
 def customise_Validation(process):
     process.load('Validation.Configuration.gemSimValid_cff')
     process.genvalid_all += process.me0SimValid
